2e18a05d090d38b110c2ecb8de44eca2cabdabb8
ccp456 <atmccp@163.com>
2018-10-05 12:49:11 +0800

    cookie

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,8 +1,15 @@
 from app import app
-from flask import render_template,url_for
+from flask import render_template,url_for,make_response,request
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username' : 'Clonwpiece'}
     return render_template('index.html',title='mine',user=user)
+
+
+@app.route('/set_cookie')
+def set_cookie():
+    resp=make_response('Hello World')
+    resp.set_cookie('Name','Hyman')
+    return resp
